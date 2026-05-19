5b3f8b5991aff21606c24fb57ca7c49b2d3ea53f
Shreyansh <jaiswal23456@gmail.com>
2026-05-19 21:11:06 +0530

    changed_Window.cpp_OOP

--- Mplayer/include/Window.h
+++ Mplayer/include/Window.h
@@ -2,17 +2,13 @@
 
 
 #include <SDL3/SDL.h>
-#include <SDL3_image/SDL_image.h>
-
-#include <string>
-
 
 class Window {
 public:
     Window();
     ~Window();
-    bool initialise(SDL_Window*& window,
-        SDL_Renderer*& renderer);
+    bool initialise();
     SDL_Window = get_window();
-private
+private:
+	SDL_Window* window; 
 };
--- Mplayer/src/Window.cpp
+++ Mplayer/src/Window.cpp
@@ -4,7 +4,14 @@
 #include <string>
 #include <iostream>
 #include "../include/Window.h"
-bool initialise(SDL_Window*& window, SDL_Renderer*& renderer) {
+Window::Window() 
+	: window(nullptr) {}
+    
+Window::~Window() {
+    if(window)SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+bool Window::initialise() {
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "INITIALISATION ERROR "
@@ -14,39 +21,30 @@ bool initialise(SDL_Window*& window, SDL_Renderer*& renderer) {
 
     window = SDL_CreateWindow("Interface", 800, 800, SDL_WINDOW_OPENGL);
 
-    if (window == NULL)
+    if (!window)
+    {    std::cout << SDL_GetError() << '\n';
         return false;
+    }
 
-    renderer = SDL_CreateRenderer(window, NULL);
-
-    if (renderer == NULL)
-        return false;
+   
 
     return true;
 }
 
-void close_window(SDL_Window* window, SDL_Renderer* renderer) {
+bool Window::processEvents() {
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    SDL_Event event;
 
-    SDL_Quit();
-}
+    while (SDL_PollEvent(&event)) {
 
-SDL_Texture* load_media(std::string path, SDL_Renderer* renderer) {
+        if (event.type == SDL_EVENT_QUIT)
+            return false;
 
-    SDL_Surface* img = IMG_Load(path.c_str());
-
-    if (img == NULL) {
-
-        std::cout << "ERROR INIT-ING THE SURFACE";
-
-        return NULL;
+        if (event.type == SDL_EVENT_KEY_DOWN &&
+            event.key.key == SDLK_ESCAPE)
+            return false;
     }
 
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, img);
-
-    SDL_DestroySurface(img);
-
-    return texture;
+    return true;
 }
+
